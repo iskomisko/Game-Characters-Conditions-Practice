30756e9eef0c6febb94a26dfa41bd7c8cb40bc1e
iskomisko <iscomorenoo@gmail.com>
2026-02-25 10:10:31 +0600

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,9 @@
+
+public class Character {
+    public void attack() {
+        System.out.println("Character attacks");
+    }
+    public void defend() {
+        System.out.println("Character defends");
+    }
+}
